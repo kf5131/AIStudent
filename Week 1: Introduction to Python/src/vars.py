d30ae68f2dab46c4e15bb93ea952becd0679f51d
kf5131 <kf5131@icloud.com>
2025-01-27 10:00:43 +0100

    make vars

---
+++ Week 1: Introduction to Python/src/vars.py
@@ -0,0 +1,12 @@
+# Contact information
+name: str = "Karl Fredrik"
+age: int = 25
+phone_number: str = "0721589630"
+email: str = "kfml2@kth.se"
+
+# Display contact information
+print("Contact Information:")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Phone Number: {phone_number}")
+print(f"Email: {email}")
