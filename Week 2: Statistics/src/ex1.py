b24ff49b5e1ad7099aa7f51b752165f792a022d4
kf5131 <kf5131@icloud.com>
2025-01-31 08:01:26 +0100

    add boilerplate

---
+++ Week 2: Statistics/src/ex1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Load the data
+data = pd.read_csv('data/Student_Scores_Data.csv')
+
+# Display the first few rows of the data
+print(data.head())
+
+# Compute descriptive statistics
+
+###################################
+# Compute the statistics here     #
+# #### TODO ####                  #
+###################################
+
+# Check out numpy's functions for calculating these statistics
+# Here: https://numpy.org/doc/stable/reference/routines.statistics.html#averages-and-variances
+
+# Visualize the data using histograms, box plots, scatter plots, and bar charts
+
+###################################
+# Visualize the data here         #
+# #### TODO ####                  #
+###################################
+
+# Check out matplotlib's functions for creating these visualizations
+# Here: https://matplotlib.org/stable/gallery/index.html
